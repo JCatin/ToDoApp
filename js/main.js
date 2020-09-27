b761f5d98f86609e15eee175e1ebcf2087207ada
JCat <andryxakulishow@gmail.com>
2020-09-28 00:47:50 +0300

    My first full-app with JS/HTML/CSS

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let inputField = document.querySelector('.inputField');
+const taskList = document.querySelector('.taskList')
+
+
+inputField.addEventListener('keydown', function(event){
+    if(event.code == 'Enter') {
+        var task = document.createElement('p');
+        task.innerText = inputField.value;
+        taskList.appendChild(task);
+
+        inputField.value = '';
+
+        task.addEventListener('click', function(){
+            task.style.textDecoration = 'line-through';
+        })
+        task.addEventListener('dblclick', function(){
+            taskList.removeChild(task);
+        })
+    }
+})
